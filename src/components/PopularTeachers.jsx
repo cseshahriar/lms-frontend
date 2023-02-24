a4c5936cfab07e2a05647a531fb16c5e458e1b59
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-24 09:26:58 +0600

    popular teacher fetch

--- src/components/PopularTeachers.jsx
+++ src/components/PopularTeachers.jsx
@@ -1,99 +1,47 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 const PopularTeachers = () => {
+    const [teachers, setTeachers] = useState(null);
+
     useEffect(() => {
-        document.title="Popular Teacher"
-    })
+        document.title="Popular Teacher";
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/teachers/`)
+            .then((response) => {
+                setTeachers(response.data);
+            });
+    }, []) // dependencies
 
     return (
         <div className='container mt-4'>
             {/* latest courses */}
             <h5 className="pd-1 mb-4">Popular Instructors </h5>
+
             <div className="row mb-4">
-                {/* single course */}
-                <div className="col-md-3 mb-4">
-                    <div className="card p-3">
-                        <Link to={`/teachers/${1}`}>
-                            <img src="teacher.png" className="card-img-top" alt="" />
-                        </Link>
+                {
+                    teachers && teachers.map((teacher) => (
+                        <div className="col-md-3 mb-4" key={teacher.id}>
+                            <div className="card p-3">
+                                <Link to={`/teachers/${1}`}>
+                                    <img src="teacher.png" className="card-img-top" alt="" />
+                                </Link>
 
-                        <div className="card-body">
-                            <h5 className="card-title text-center">
-                                <Link to={`/teachers/${1}`}>Name</Link>
-                            </h5>
-                        </div>
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">
+                                        <Link to={`/teachers/${1}`}>{teacher.full_name}</Link>
+                                    </h5>
+                                </div>
 
-                        <div className='card-footer'>
-                            <div className='title'>
-                                <span>Rating: 4.5/5 </span>  
+                                <div className='card-footer'>
+                                    <div className='title'>
+                                        <span>Rating: 4.5/5 </span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                
-                {/* single course */}
-                <div className="col-md-3 mb-4">
-                    <div className="card p-3">
-                        <Link to={`/teachers/${1}`}>
-                            <img src="teacher.png" className="card-img-top" alt="" />
-                        </Link>
-
-                        <div className="card-body">
-                            <h5 className="card-title text-center">
-                                <Link to={`/teachers/${1}`}>Name</Link>
-                            </h5>
-                        </div>
-
-                        <div className='card-footer'>
-                            <div className='title'>
-                                <span>Rating: 4.5/5 </span>  
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* single course */}
-                <div className="col-md-3 mb-4">
-                    <div className="card p-3">
-                        <Link to={`/teachers/${1}`}>
-                            <img src="teacher.png" className="card-img-top" alt="" />
-                        </Link>
-
-                        <div className="card-body">
-                            <h5 className="card-title text-center">
-                                <Link to={`/teachers/${1}`}>Name</Link>
-                            </h5>
-                        </div>
-
-                        <div className='card-footer'>
-                            <div className='title'>
-                                <span>Rating: 4.5/5 </span>  
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* single course */}
-                <div className="col-md-3 mb-4">
-                    <div className="card p-3">
-                        <Link to={`/teachers/${1}`}>
-                            <img src="teacher.png" className="card-img-top" alt="" />
-                        </Link>
-
-                        <div className="card-body">
-                            <h5 className="card-title text-center">
-                                <Link to={`/teachers/${1}`}>Course title</Link>
-                            </h5>
-                        </div>
-
-                        <div className='card-footer'>
-                            <div className='title'>
-                                <span>Rating: 4.5/5 </span>  
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
             {/* End latest courses */}
 

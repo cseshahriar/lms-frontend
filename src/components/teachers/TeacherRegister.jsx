8948f7b48dec457419cdf1fa547280b16415b053
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-24 08:00:33 +0600

    teacher register form update

--- src/components/teachers/TeacherRegister.jsx
+++ src/components/teachers/TeacherRegister.jsx
@@ -15,30 +15,34 @@ const TeacherRegister = () => {
                                 </div>
 
                                 <div className="mb-3">
+                                    <label htmlFor="username" className="form-label">Qualification</label>
+                                    <input type="text" className="form-control" name='qualification'
+                                           id="qualification"/>
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="mobile_no" className="form-label">Mobile Number</label>
+                                    <input type="text" className="form-control" name='mobile_no' id="mobile_no"/>
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="skills" className="form-label">Skills</label>
+                                    <textarea name="skills" className="form-control" id="skills"></textarea>
+                                    <div id="skillsHelp" className="form-text">PHP, Python, JavaScript</div>
+                                </div>
+
+                                <div className="mb-3">
                                     <label for="exampleInputEmail1" className="form-label">Email</label>
-                                    <input type="email" className="form-control" id="email"/>
-                                    
+                                    <input type="email" className="form-control" name='email' id="email"/>
                                 </div>
 
                                 <div className="mb-3">
-                                    <label for="username" className="form-label">Username</label>
-                                    <input type="text" className="form-control" id="username" />
-                                </div>
-                                
-
-                                <div className="mb-3">
-                                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                                </div>
-
-                                <div className="mb-3">
-                                    <label for="interest" className="form-label">Skills</label>
-                                    <textarea name="interest"  className="form-control" id="interest"></textarea>
-                                    <div id="emailHelp" className="form-text">PHP, Python, JavaScript</div>
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" name='Password' className="form-control" id="Password"/>
                                 </div>
 
                                 <div className="mb-3 text-center">
-                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                    <button type="submit" className="btn btn-success">Register</button>
                                 </div>
                             
                             </form>

b942b2f1ef99860dd866c34e9c2245d8d52e10d8
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-26 03:00:42 +0600

    course create component

--- src/components/teachers/AddCourse.jsx
+++ src/components/teachers/AddCourse.jsx
@@ -3,20 +3,97 @@ import TeacherSidebar from "./TeacherSidebar";
 import { isTeacherAuthenticated } from "../../functions";
 
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const AddCourse = () => {
+    const navigate = useNavigate();
+
+    // states
+    const [error, setError] = useState(null);
+    const [ isAlertVisible, setIsAlertVisible ] = React.useState(false);
     const [categories, setCategories] = useState([]);
+
+    const [courseData, setCourseData] = useState({
+        'category': '',
+        'teacher': '',
+        'title': '',
+        'description': '',
+        'technologies': '',
+        'featured_img': '',
+        'status': ''
+    });
 
     useEffect(() => {
         // auth check
         isTeacherAuthenticated();
+        try {
+            axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/categories/`)
+                .then(response => {
+                    setCategories(response.data)
+                })
+        } catch (error) {
+            console.log(error);
+        }
+    }, [ ])
 
-        axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/categories/`)
-            .then(response => setCategories(response.data))
-    })
+    const handleChange = (event) => {
+        setCourseData({
+            ...courseData, [event.target.name]: event.target.value
+        })
+    }
+
+    const handleFileChange = (event) => {
+        setCourseData({
+            ...courseData, [event.target.name]: event.target.files[0]
+        })
+    }
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        const _formData = new FormData();
+        _formData.append('category', courseData.category)
+        _formData.append('teacher', 20)
+        _formData.append('title', courseData.title)
+        _formData.append('description', courseData.description)
+        _formData.append('featured_img', courseData.featured_img)
+        _formData.append('technologies', courseData.technologies)
+
+        console.log(_formData)
+
+        try {
+            axios.post(
+                `${process.env.REACT_APP_API_BASE_URL}/api/courses/`,
+                _formData,
+                {
+                    headers: {
+                        'content-type': 'multipart/form-data'
+                    }
+                }
+            ).then((response) => {
+                console.log(response.data)
+                setCourseData({
+                    'category': '',
+                    'teacher': '',
+                    'title': '',
+                    'description': '',
+                    'course_video': '',
+                    'technologies': '',
+                    'featured_img': null,
+                    'status': 'success'
+                })
+                setIsAlertVisible(true);
+                setTimeout(function () {
+                    setIsAlertVisible(false);
+                    navigate('/teacher-courses');
+                }, 3000);
+            })
+        } catch (error) {
+            setError(error);
+            setCourseData({'status': 'error'});
+        }
+    }
 
     return (
-
         <div className='container py-5'>
             <div className='row'>
                 {/* aside */}
@@ -27,14 +104,20 @@ const AddCourse = () => {
                 {/* content */}
                 <section className='col-md-9'>
                     <div className='card'>
+                        {
+                            isAlertVisible &&
+                            courseData.status == 'success' && <p className="text-success" id="success">Thanks for your registration</p>
+                        }
+                        { courseData.status == 'error' && <p className="text-danger">Something went wrong! Please try again.</p>}
+
+
                         <h5 className="card-header">Add Course</h5>
                         <div className='card-body'>
                             <form>
                                 <div className="mb-3 row">
                                     <label htmlFor="category" className="col-sm-2 col-form-label">Category</label>
                                     <div className="col-sm-10">
-                                        <select className="form-select" name='category'>
-                                            <option selected>Select Category</option>
+                                        <select className="form-select" name='category' onChange={handleChange}>
                                             {
                                                 categories && categories.map((category) => (
                                                     <option key={category.id} value={category.id}>{category.title}</option>
@@ -47,33 +130,33 @@ const AddCourse = () => {
                                 <div className="mb-3 row">
                                     <label htmlFor="title" className="col-sm-2 col-form-label">Title</label>
                                     <div className="col-sm-10">
-                                        <input type="text" className="form-control" id="title"/>
+                                        <input type="text" className="form-control" id="title" name='title' value={courseData.title} onChange={handleChange} />
                                     </div>
                                 </div>
 
                                 <div className="mb-3 row">
                                     <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
                                     <div className="col-sm-10">
-                                        <textarea name="description" className="form-control" id="description"></textarea>
+                                        <textarea name="description" className="form-control" id="description" name='description' value={courseData.description} onChange={handleChange}></textarea>
                                     </div>
                                 </div>
 
                                 <div className="mb-3 row">
-                                    <label htmlFor="photo" className="col-sm-2 col-form-label">Course Video</label>
+                                    <label htmlFor="featured_img" className="col-sm-2 col-form-label">Featured Image</label>
                                     <div className="col-sm-10">
-                                        <input type="file" name="course_video" className="form-control" id="course_video"/>
+                                        <input type="file" name="featured_img" className="form-control" id="featured_img" name='course_video'  onChange={handleFileChange} />
                                     </div>
                                 </div>
 
                                 <div className="mb-3 row">
                                     <label htmlFor="technologies" className="col-sm-2 col-form-label">Technologies</label>
                                     <div className="col-sm-10">
-                                        <textarea name="technologies" className="form-control" id="technologies"></textarea>
+                                        <textarea name="technologies" className="form-control" id="technologies" name='technologies' value={courseData.technologies} onChange={handleChange}></textarea>
                                     </div>
                                 </div>
 
                                 <div className="mb-3 text-center">
-                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                    <button onClick={formSubmit} type="submit" className="btn btn-primary">Submit</button>
                                 </div>
                             </form>
                         </div>

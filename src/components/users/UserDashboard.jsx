57809aae218bb57de85249803913249718156c23
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-04-03 17:33:37 +0600

    user dashboard

--- src/components/users/UserDashboard.jsx
+++ src/components/users/UserDashboard.jsx
@@ -1,13 +1,27 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Sidebar from './Sidebar';
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const UserDashboard = () => {
     const navigate = useNavigate();
     const studentLoginStatus = localStorage.getItem('studentLoginStatus');
+    const student_id = localStorage.getItem('student_id');
+    const [student, setStudent] = useState();
 
     useEffect(() => {
         document.title="User Dashboard";
+
+        // total_enrolled_students
+        axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/students/${student_id}`,
+        )
+        .then((response) => {
+            setStudent(response.data)
+        })
+        .catch((errors) => {
+            console.log(errors.response.data)
+        })
 
         // login check
         if(studentLoginStatus !== 'true') {
@@ -23,9 +37,54 @@ const UserDashboard = () => {
                 </aside>
                 
                 <section className='col-md-9'>
-                    Dashboard
-                </section>
+                    <div className='row'>
 
+                        <div className="col-md-4">
+                            <div className="card text-center">
+                                <div className="card-header bg-primary text-white">
+                                    Enrolled Courses
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        <Link to='/'>{ student && student.total_enrolled_students }</Link>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card text-center">
+                                <div className="card-header bg-success text-white">
+                                    Favorite Courses
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        <Link to='/'>{ student && student.total_favorite_students }</Link>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card text-center">
+                                <div className="card-header bg-info text-white">
+                                    Assignments
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        <Link to='/' className='text-success' title='Completed'>
+                                            { student && student.total_complete_assignments }
+                                        </Link>
+
+                                        <Link to='/' className='text-secondary ms-1' title='Pending'>
+                                            { student && student.total_pending_assignments }
+                                        </Link>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     );
